3176d8e2dadb43974d8cc3a27984b8cfb120d363
Zhakyp <73534500+Zhakyp01@users.noreply.github.com>
2023-04-09 18:16:54 +0600

    Update variables.tf

--- modules/postgresql/variables.tf
+++ modules/postgresql/variables.tf
@@ -291,13 +291,13 @@ variable "additional_databases" {
 variable "user_name" {
   description = "The name of the default user"
   type        = string
-  default     = "default"
+  default     = "postgres"
 }
 
 variable "user_password" {
   description = "The password for the default user. If not set, a random one will be generated and available in the generated_user_password output variable."
   type        = string
-  default     = ""
+  default     = "postgres"
 }
 
 variable "additional_users" {
